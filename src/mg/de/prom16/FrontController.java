ec86e27d3198096f11628cefc23bb77739e7881c
Alberto2473 <169131884+Alberto2473@users.noreply.github.com>
2024-12-14 07:42:28 +0100

    Sprint6

--- src/mg/de/prom16/FrontController.java
+++ src/mg/de/prom16/FrontController.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -13,6 +15,7 @@ import MV.ModelView;
 import Map.Mapping;
 import annotation.AnnotationController;
 import annotation.GetMethode;
+import annotation.RequestParam;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -53,13 +56,15 @@ public class FrontController extends HttpServlet
                                     Method[] method = classe.getMethods();
                                     ArrayList<String> methodName = new ArrayList<>();
                                     ArrayList<String> annotationMethod = new ArrayList<>();
-                                    ArrayList<String> typeClasse=new ArrayList<>();
+                                    ArrayList<String> typeClasse = new ArrayList<>();
+                                    ArrayList<Method> listeMethode = new ArrayList<>();
                                     for (int j = 0; j < method.length; j++) {
                                         if (method[j].isAnnotationPresent(GetMethode.class)) {
                                             GetMethode annotation = method[j].getAnnotation(GetMethode.class);
                                             methodName.add(method[j].getName());
                                             annotationMethod.add(annotation.value());
                                             typeClasse.add(method[j].getReturnType().getSimpleName());
+                                            listeMethode.add(method[j]);
                                         }
                                     }
 
@@ -67,18 +72,20 @@ public class FrontController extends HttpServlet
                                     HashMap<String, Mapping> hashMap = new HashMap<>();
                                     hashMap.put(requestedPage, map);
 
-                                    int indiceAssociation = this.associer(requestedPage, annotationMethod);
-                                    if (indiceAssociation != -1) {
-                                        if (typeClasse.get(indiceAssociation).equals("String")) {
-                                            String execution = String.valueOf(this.execMethod(classe,hashMap.get(requestedPage).getMethodName().get(indiceAssociation)));
+                                    ArrayList<String> listParamName = new ArrayList<>();
+                                    ArrayList<String> listParamValue = new ArrayList<>();
 
-                                            out.println("Classe: " + classe.getSimpleName());
-                                            out.println("Method: " + hashMap.get(requestedPage).getMethodName().get(indiceAssociation));
-                                            out.println("Reponse de l'execution: " + execution);    
-                                        }
-                                        else {
-                                            Object object2=this.execMethod(classe,hashMap.get(requestedPage).getMethodName().get(indiceAssociation));
-                                            
+                                    this.getParamNameAndValue(listParamName, listParamValue, request);
+
+                                    int indiceAssociation = this.associer(requestedPage, annotationMethod);
+
+                                    if (indiceAssociation > -1) {
+                                        Method methodSimple = this.getMethodSimple(hashMap.get(requestedPage).getMethodName().get(indiceAssociation),listeMethode);
+                                        Parameter[] parameters = methodSimple.getParameters();
+                                        Object[] objectParameter = this.listeObjet(listParamName, listParamValue,parameters);
+                                        if (objectParameter.length>0) {
+                                            Object object2 = this.execMethodParams(classe, hashMap.get(requestedPage).getMethodName().get(indiceAssociation),objectParameter);
+
                                             ModelView modelView = new ModelView();
                                             modelView = (ModelView) object2;
 
@@ -88,7 +95,33 @@ public class FrontController extends HttpServlet
                                                 request.setAttribute(key, value);
                                             }
 
+                                            // out.println("url: "+modelView.getUrl());
+                                            // out.println(request.getAttribute("reponse"));
+
                                             request.getRequestDispatcher(modelView.getUrl()).forward(request, response);
+                                        }
+                                        else {
+                                            if (typeClasse.get(indiceAssociation).equals("String")) {
+                                                String execution = String.valueOf(this.execMethod(classe,hashMap.get(requestedPage).getMethodName().get(indiceAssociation)));
+
+                                                out.println("Classe: " + classe.getSimpleName());
+                                                out.println("Method: " + hashMap.get(requestedPage).getMethodName().get(indiceAssociation));
+                                                out.println("Reponse de l'execution: " + execution);    
+                                            }
+                                            else {
+                                                Object object2=this.execMethod(classe,hashMap.get(requestedPage).getMethodName().get(indiceAssociation));
+                                                
+                                                ModelView modelView = new ModelView();
+                                                modelView = (ModelView) object2;
+
+                                                for (Entry<String, Object> entry : modelView.getHashMap().entrySet()) {
+                                                    String key = entry.getKey();
+                                                    Object value = entry.getValue();
+                                                    request.setAttribute(key, value);
+                                                }
+
+                                                request.getRequestDispatcher(modelView.getUrl()).forward(request, response);
+                                            }
                                         }
                                         
                                         // Quitter le scan des classes et confirmer la liaison  de l'url et le methode
@@ -127,10 +160,76 @@ public class FrontController extends HttpServlet
     }
 
     //Sprint3
-    protected Object execMethod(Class<?> cla, String methodName) throws Exception {
+    public Object execMethod(Class<?> cla, String methodName) throws Exception {
         Method method = cla.getMethod(methodName);
         Object controllerInstance = cla.getDeclaredConstructor().newInstance();
         Object result = method.invoke(controllerInstance);
+        return result;
+    }
+
+    // Sprint 6
+
+    public void getParamNameAndValue(ArrayList<String> listParamName, ArrayList<String> listParamValue,
+            HttpServletRequest request) {
+        Enumeration<String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String paramName = paramNames.nextElement();
+            String paramValue = request.getParameter(paramName);
+
+            listParamName.add(paramName);
+            listParamValue.add(paramValue);
+        }
+    }
+
+    // Prendre en tant que type Method le methode lies a l'url 
+    public Method getMethodSimple(String methodeAssocie, ArrayList<Method> listeMethode) {
+        Method methodSimple = null;
+        for (int l = 0; l < listeMethode.size(); l++) {
+            if (methodeAssocie.equals(listeMethode.get(l).getName())) {
+                methodSimple = listeMethode.get(l);
+            }
+        }
+        return methodSimple;
+    }
+    
+    public Object[] listeObjet(ArrayList<String> listParamName, ArrayList<String> listParamValue,
+            Parameter[] parameters) {
+        ArrayList<Object> listeObjet = new ArrayList<>();
+
+        for (Parameter parameter : parameters) {
+            if (parameter.isAnnotationPresent(RequestParam.class)) {
+                RequestParam annotation2 = parameter.getAnnotation(RequestParam.class);
+                for (int j = 0; j < listParamName.size(); j++) {
+                    if (annotation2.value().equals(listParamName.get(j))) {
+                        listeObjet.add(listParamValue.get(j));
+                    }
+                }
+            }
+        }
+
+        // Caster la liste d'objet en tableau d'objet pour l'execution de methode
+        Object[] objectParameter = new Object[listeObjet.size()];
+        for (int index = 0; index < objectParameter.length; index++) {
+            objectParameter[index] = listeObjet.get(index);
+        }
+
+        return objectParameter;
+    }
+
+    protected Object execMethodParams(Class<?> cla, String methodName, Object[] param) throws Exception {
+        Class<?>[] paramTypes = new Class<?>[param.length];
+
+        for (int i = 0; i < param.length; i++) {
+            if (param[i] == null) {
+                paramTypes[i] = Object.class;
+            } else {
+                paramTypes[i] = param[i].getClass();
+            }
+        }
+        Method method = cla.getMethod(methodName, paramTypes);
+        Object controllerInstance = cla.getDeclaredConstructor().newInstance();
+        Object result = method.invoke(controllerInstance, param);
+
         return result;
     }
 
